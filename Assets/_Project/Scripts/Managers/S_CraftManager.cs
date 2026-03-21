b7ac7faf513cc623e4657e41ff0ba2f4313bc25d
raaaahman <sylvain@devindetails.com>
2026-03-21 16:03:31 +0100

    [ADD] S_CraftManager

---
+++ Assets/_Project/Scripts/Managers/S_CraftManager.cs
@@ -0,0 +1,81 @@
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using UnityEngine;
+
+public class CraftManager : MonoBehaviour
+{
+    [Range(1.0f, 4.0f)]
+    [SerializedField] private float _snapRadius;
+    private int _currentTabletIndex = 0;
+    const int NUM_TABLETS = 3;
+
+    private int _selectedSymbolId = -1;
+    private float _currentSymbolScale = 1f;
+    private float _currentRotation = 0f;
+    private int _selectedMaterialId = -1;
+
+    private List<PlacedSymbol> PlacedSymbols = [];
+
+    public void NextTablet()
+    {
+        _currentTabletIndex = (_currentTabletIndex + 1) % NUM_TABLETS;
+    }
+
+    public void PreviousTablet()
+    {
+        _currentTabletIndex--;
+        if (_currentTabletIndex < 0) _currentTabletIndex = NUM_TABLETS - 1;
+    }
+
+    public void SelectSymbol(int id)
+    {
+        _selectedSymbolId = id;
+    }
+
+    public void ResizeSymbol(float factor)
+    {
+        if (_selectedSymbolId < 0) return;
+
+        _currentSymbolScale = Math.Clamp(_currentSymbolScale + factor, 0.5f, 2f);
+    }
+
+    public void RotateSymbol(float factor)
+    {
+        if (_selectedSymbolId) return;
+
+        _currentRotation = (_currentRotation + factor) % 360f;
+        if (_currentRotation < 0) _currentRotation = 360f - _currentRotation;
+    }
+
+    public void UnselectSymbol()
+    {
+        _selectedSymbolId = -1;
+        _currentSymbolScale = 1f;
+        _currentRotation = 0f;
+    }
+
+    public void PlaceSymbol(Vector2 position)
+    {
+        if (_selectedSymbolId >= 0 && PlacedSymbols.Count < 128)
+            PlacedSymbols.Add(new PlacedSymbol { Id = _selectedSymbolId, Position = position, Scale = _currentSymbolScale });
+
+        UnselectSymbol();
+    }
+
+    public void PlaceMaterial(Vector2 position)
+    {
+        float minDist = float.MaxValue;
+        PlacedSymbol closest = null;
+        foreach (var zone in PlacedSymbols)
+        {
+            float dist = Vector2.Distance(position, zone.position);
+            if (dist < minDist && dist <= _snapRadius)
+            {
+                minDist = dist;
+                closest = zone;
+            }
+        }
+        closest.MaterialId = _selectedMaterialId;
+    }
+}
---
+++ Assets/_Project/Scripts/Utils/S_PlacedSymbol.cs
@@ -0,0 +1,8 @@
+public class PlacedSymbol
+{
+    public int Id;
+    public Vector2 Position;
+    public float Scale;
+    public float Rotation;
+    public int MaterialId;
+}
