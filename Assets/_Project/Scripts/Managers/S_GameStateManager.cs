e329927199a13ffd6939f99031387546a127d375
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2026-03-21 11:18:37 +0100

    [ADD] GameStates

---
+++ Assets/_Project/Scripts/Managers/S_GameStateManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class S_GameStateManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        PAUSEMENU,
+        SELECTCLIENT,
+        CLIENTSPAWN,
+        ITEMDELIVERY,
+        REWARD
+    }
+}
