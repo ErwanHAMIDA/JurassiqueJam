8dbd70f2ef7c37649fe7f0185245c9e941451e18
raaaahman <sylvain@devindetails.com>
2026-03-22 07:54:43 +0100

    Use GameStateManager to switch between UIs

--- Assets/_Project/Scripts/Managers/S_GameStateManager.cs
+++ Assets/_Project/Scripts/Managers/S_GameStateManager.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class S_GameStateManager : MonoBehaviour
 {
+
     [SerializeField] private GameObject _pauseMenuUI;
+    [SerializeField] private GameObject _hubMenuUI;
+    [SerializeField] private GameObject _craftMenuUI;
+    [SerializeField] private GameObject _craftManager;
+
     public enum GameState
     {
         PAUSEMENU,
         SELECTCLIENT,
         CLIENTSPAWN,
+        WORKSHOPOVERVIEW,
+        ITEMCRAFTING,
         ITEMDELIVERY,
         REWARD
     }
 
+    private GameState _previousGameState;
     private GameState _currentGameState;
 
     private bool _isPaused = true;
@@ -22,23 +30,39 @@ public class S_GameStateManager : MonoBehaviour
     }
     public void ChangeState(int state)
     {
-        _currentGameState = (GameState)state;
-
-        switch (_currentGameState)
+        _previousGameState = (GameState)state;
+        
+        switch (state)
         {
-            case (GameState.PAUSEMENU):
+            case (int)GameState.PAUSEMENU:
+                _pauseMenuUI.SetActive(true);
+
                 break;
-            case (GameState.SELECTCLIENT):
+            case (int)GameState.SELECTCLIENT:
+                _hubMenuUI.SetActive(true);
+                _craftMenuUI.SetActive(false);
                 break;
-            case (GameState.CLIENTSPAWN):
+            case (int)GameState.CLIENTSPAWN:
+                
                 break;
-            case (GameState.ITEMDELIVERY):
+            case (int)GameState.WORKSHOPOVERVIEW:
+                _hubMenuUI.SetActive(false);
+                _craftMenuUI.SetActive(true);
+                _craftManager.SetActive(false);
                 break;
-            case (GameState.REWARD):
+            case (int)GameState.ITEMCRAFTING:
+                _craftManager.SetActive(true);
+                break;
+            case (int)GameState.ITEMDELIVERY:
+                _craftMenuUI.SetActive(false);
+                _hubMenuUI.SetActive(true);
+                _craftManager.SetActive(false);
+                break;
+            case (int)GameState.REWARD:
                 break;
         }
 
-        //_previousGameState = (GameState)state;
+        _currentGameState = (GameState)state;
     }
 
     
