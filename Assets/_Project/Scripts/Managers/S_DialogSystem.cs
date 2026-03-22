8a6037907c5bf6fe2471f7e96e7c4f47bf41b8d8
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2026-03-22 12:29:05 +0100

    [ADD] Quit Button for client

--- Assets/_Project/Scripts/Managers/S_DialogSystem.cs
+++ Assets/_Project/Scripts/Managers/S_DialogSystem.cs
@@ -8,6 +8,10 @@ public class S_DialogSystem : MonoBehaviour
     [SerializeField] List<SO_ClientDialog> _allclientSentencesList;
     [SerializeField] TextMeshProUGUI _dialogText;
     [SerializeField] S_ClientManager _clientManager;
+    [SerializeField] GameObject _previousButton;
+    [SerializeField] GameObject _nextButton;
+    [SerializeField] GameObject _endDialogButton;
+    [SerializeField] GameObject _dialogPanel;
 
     private int _currentIndex = 0;
 
@@ -30,5 +34,28 @@ public class S_DialogSystem : MonoBehaviour
     public void UpdateDialog()
     {
         _dialogText.text = _allclientSentencesList[_clientManager.GetCurrentClientID()]._clientSentences[_currentIndex];
+
+        Debug.Log(_currentIndex);
+        if (_currentIndex == 0)
+        {
+            _previousButton.gameObject.SetActive(false);
+        }
+
+        if (_currentIndex == _allclientSentencesList[_clientManager.GetCurrentClientID()]._clientSentences.Count - 1)
+        {
+            _nextButton.gameObject.SetActive(false);
+            _endDialogButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            _nextButton.gameObject.SetActive(true);
+            _previousButton.gameObject.SetActive(true);
+            _endDialogButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void EndDialog()
+    {
+        _dialogPanel.gameObject.SetActive(false);
     }
 }
