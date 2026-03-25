3b1055f37ded5ea38932306454ed1bd3667472d1
Rafael-Omalian <pro.adrien.paul@gmail.com>
2026-03-25 12:04:01 +0100

    Fix infinite looping not working correctly with window resolution

--- Assets/_Project/Scripts/UI/S_SymbolInfiniteMovement.cs
+++ Assets/_Project/Scripts/UI/S_SymbolInfiniteMovement.cs
@@ -1,25 +1,29 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class S_SymbolInfiniteMovement : MonoBehaviour
 {
     private float _minimumRotation;
     private float _maximumRotation;
+    private RectTransform _rectTransform;
 
     void Start()
     {
-        _minimumRotation = CraftManager.Instance._baseTexture.parent.position.x - CraftManager.Instance._maximumRotation;
-        _maximumRotation = CraftManager.Instance._baseTexture.parent.position.x + CraftManager.Instance._maximumRotation;
+        _rectTransform = GetComponent<RectTransform>();
+        _minimumRotation = CraftManager.Instance._baseTexture.parent.GetComponent<RectTransform>().position.x - CraftManager.Instance._maximumRotation;
+        _maximumRotation = CraftManager.Instance._baseTexture.parent.GetComponent<RectTransform>().position.x + CraftManager.Instance._maximumRotation;
     }
 
     void Update()
     {
-        if (transform.position.x < _minimumRotation)
-        {
-            transform.position = new Vector3(_maximumRotation + (transform.position.x - _minimumRotation) , transform.position.y, transform.position.z);
+        if (Screen.width < 100) return;
+        if (_rectTransform.position.x < _minimumRotation * (Screen.width / 1920.0f))
+        { 
+            _rectTransform.position = new Vector3(_maximumRotation * (Screen.width / 1920.0f) + (_rectTransform.position.x - _minimumRotation * (Screen.width / 1920.0f)) , _rectTransform.position.y, _rectTransform.position.z);
         }
-        else if (transform.position.x > _maximumRotation)
+        else if (_rectTransform.position.x > _maximumRotation * (Screen.width / 1920.0f))
         {
-            transform.position = new Vector3(_minimumRotation + (transform.position.x - _maximumRotation), transform.position.y, transform.position.z);
+            _rectTransform.position = new Vector3(_minimumRotation * (Screen.width / 1920.0f) + (_rectTransform.position.x - _maximumRotation * (Screen.width / 1920.0f)), _rectTransform.position.y, _rectTransform.position.z);
         }
     }
 }
