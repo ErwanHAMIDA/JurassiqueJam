dccfbebe51ce53ddd33e471dab623dd0db83d2df
raaaahman <sylvain@devindetails.com>
2026-05-12 16:59:08 +0200

    [CHANGE] Use 16:9 screen ratio for web export [CHANGE] Use blurred workshop background for loading screen [FIX] Remove unused editor dependecies in runtime code

--- Assets/_Project/Scripts/Managers/S_ClientManager.cs
+++ Assets/_Project/Scripts/Managers/S_ClientManager.cs
@@ -1,9 +1,7 @@
 using JetBrains.Annotations;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 
